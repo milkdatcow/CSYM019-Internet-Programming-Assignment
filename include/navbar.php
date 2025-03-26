3aad4da3f2ab438e32dad2cda5c56d2e4287f45b
Dyl <bowyer008@gmail.com>
2025-03-26 10:58:03 +0000

    Added homepage and stylesheet

---
+++ include/navbar.php
@@ -0,0 +1,12 @@
+<?php
+
+echo'
+<div class="nav">
+    <a href = "#">Events</a>
+    <a href = "#">Admin</a>
+    <a href = "#">Login</a>
+</div>
+';
+
+
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'include/head.php'; ?>
+<body>
+    <?php include 'include/navbar.php';?>
+    <h1>Hello</h1>
+    <div class="container">
+        <div class="item"><p>Test1</p><a href="">Book</a></div>
+        <div class="item"><p>Test2</p></div>
+        <div class="item"><p>Test3</p></div>
+        <div class="item"><p>Test4</p></div>        
+    </div>
+</body>
+</html>
