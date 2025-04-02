fd8a7e865a8bfe6f798d9e73353c43ceaf0683a1
Dyl <bowyer008@gmail.com>
2025-04-02 11:08:41 +0100

    Updated Homepage
    Changed styling,
    Added images and logos,
    Created footer.

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+<?php
+
+echo'
+<div class="footer">
+    <a href = "https://github.com/milkdatcow/CSYM019-Assignment"><img src="assets/github.svg" alt="logo" class="logo"></a>
+</div>
+';
+
+
+?>
--- index.php
+++ index.php
@@ -3,12 +3,28 @@
 <?php include 'include/head.php'; ?>
 <body>
     <?php include 'include/navbar.php';?>
-    <h1>Hello</h1>
+    <div class="container"><h1>Featured Events</h1></div>
+    
     <div class="container">
-        <div class="item"><p>Test1</p><a href="">Book</a></div>
-        <div class="item"><p>Test2</p></div>
-        <div class="item"><p>Test3</p></div>
-        <div class="item"><p>Test4</p></div>        
+        <div class="grid">
+            <div class="item">
+                <img src="assets/placeholder.webp" alt="placeholder">
+                <p>Event Name</p>
+                <p>Price</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in purus accumsan, facilisis lectus at, ultrices elit. Duis eget imperdiet neque, ac eleifend nulla. Curabitur id iaculis tortor. Morbi ornare erat eleifend, luctus turpis eu, faucibus nulla. Donec faucibus augue ut dolor consectetur, eu scelerisque ex pretium. Cras condimentum sodales ultricies.</p>
+                <a href="" class="btn">Book</a>
+            </div>
+            <div class="item">
+                <img src="assets/placeholder.webp" alt="placeholder">
+                <p>Test1</p>
+                <a href="">Book</a>
+            </div>
+        </div>
     </div>
+    <div class="container">
+        <p style="height:400px"></p>
+    </div>
+
+    <?php include 'include/footer.php';?>
 </body>
 </html>
