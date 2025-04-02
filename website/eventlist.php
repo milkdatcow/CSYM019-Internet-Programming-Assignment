06bf413c0460e1ef4066625ccc61155fc13afbce
Dyl <bowyer008@gmail.com>
2025-04-02 12:52:27 +0100

    Added event list page

---
+++ website/eventlist.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'include/head.php'; ?>
+<body>
+    <?php include 'include/navbar.php';?>
+    <div class="container"><h1>Events</h1></div>
+    
+    <div class="container">
+        <div class="gridlist">
+            <div class="item">
+                <img src="assets/placeholder.webp" alt="placeholder">
+                <p>Event Name</p>
+                <p>Price</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in purus accumsan, facilisis lectus at, ultrices elit. </p>
+            </div>
+            <div class="item">
+                <img src="assets/placeholder.webp" alt="placeholder">
+                <p>Event Name</p>
+                <p>Price</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in purus accumsan, facilisis lectus at, ultrices elit. </p>
+            </div>
+            <div class="item">
+                <img src="assets/placeholder.webp" alt="placeholder">
+                <p>Event Name</p>
+                <p>Price</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in purus accumsan, facilisis lectus at, ultrices elit. </p>
+            </div>
+            <div class="item">
+                <img src="assets/placeholder.webp" alt="placeholder">
+                <p>Event Name</p>
+                <p>Price</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in purus accumsan, facilisis lectus at, ultrices elit. </p>
+            </div>
+            <div class="item">
+                <img src="assets/placeholder.webp" alt="placeholder">
+                <p>Event Name</p>
+                <p>Price</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in purus accumsan, facilisis lectus at, ultrices elit. </p>
+            </div>
+            <div class="item">
+                <img src="assets/placeholder.webp" alt="placeholder">
+                <p>Event Name</p>
+                <p>Price</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in purus accumsan, facilisis lectus at, ultrices elit. </p>
+            </div>
+        </div>
+    </div>
+    <div style="min-height:50px"></div>
+    <?php include 'include/footer.php';?>
+</body>
+</html>
--- website/include/navbar.php
+++ website/include/navbar.php
@@ -3,7 +3,7 @@
 echo'
 <div class="nav">
     <a href = "index.php"><img src="assets/logo.png" alt="logo" class="logo"></a>
-    <a href = "#">Events</a>
+    <a href = "eventlist.php">Events</a>
     <a href = "#">Admin</a>
     <a href = "#">Login</a>
 </div>
