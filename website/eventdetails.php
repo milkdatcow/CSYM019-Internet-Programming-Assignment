03424f3f0a2e06139d38b7c8d5d9cb382130bd02
Dyl <bowyer008@gmail.com>
2025-04-02 12:42:56 +0100

    Added event details page

--- website/eventdetails.php
+++ website/eventdetails.php
@@ -3,24 +3,19 @@
 <?php include 'include/head.php'; ?>
 <body>
     <?php include 'include/navbar.php';?>
-    <div class="container"><h1>Featured Events</h1></div>
+    <div class="container"><h1>Event</h1></div>
     
     <div class="container">
         <div class="grid">
-            <div class="item">
-                <img src="assets/placeholder.webp" alt="placeholder">
-                <p>Event Name</p>
-                <p>Price</p>
+                <img src="assets/placeholder.webp" alt="placeholder" class="leftimage">
+            <div class="right">
+                <h1>Event Name</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in purus accumsan, facilisis lectus at, ultrices elit. Duis eget imperdiet neque, ac eleifend nulla. Curabitur id iaculis tortor. Morbi ornare erat eleifend, luctus turpis eu, faucibus nulla. Donec faucibus augue ut dolor consectetur, eu scelerisque ex pretium. Cras condimentum sodales ultricies.</p>
-            </div>
-            <div class="item">
-                <img src="assets/placeholder.webp" alt="placeholder">
-                <p>Test1</p>
-                <a href="">Book</a>
+                <button class="btn">Book</button>
             </div>
         </div>
     </div>
-    <div style="min-height:50px"></div>
+    <div style="min-height:100px"></div>
     <?php include 'include/footer.php';?>
 </body>
 </html>
